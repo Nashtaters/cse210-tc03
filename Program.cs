2fa1b1a62b06b416d2336b61c350135662f70553
Kennion Gubler <gublerkennion@gmail.com>
2021-09-29 13:29:19 -0600

    Wrote new welcome message

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace cse210_tc03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to our collaborative program!");
+        }
+
+        
+        
+        
+        
+        
+        
+        static void DisplayGreetingKennion(){
+            Console.WriteLine("reH ponglu'choH je!");
+        }
+
+    }
+}
