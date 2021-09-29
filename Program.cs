a9e8387867304ceb24923317e00d0db44ca6fc5c
Curtis Nash <91074103+Nashtaters@users.noreply.github.com>
2021-09-29 13:42:42 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace cse210_tc03
 {
@@ -9,6 +9,10 @@ namespace cse210_tc03
             Console.WriteLine("Welcome to our collaborative program!");
 
             DisplayGreetingNash();
+            
+            DisplayGreetingKennion();
+            
+            elijahWilson();
 
         }
 
@@ -19,8 +23,26 @@ namespace cse210_tc03
 
         }
         
-        static void DisplayGreetingKennion(){
+        static void DisplayGreetingKennion()
+        {
             Console.WriteLine("reH ponglu'choH je!");
+        }
+        static void elijahWilson()
+        {
+            
+            Console.Write("Wassup guys! Scale of 1-10 how familiar are you guys with these processes? ");
+            string response = Console.ReadLine();
+            int responseI = int.Parse(response);
+            if (responseI >= 5)
+            {
+                Console.Writeline("Keep up the great work!");
+            }
+            else
+            {
+                Console.WriteLine("Keep working at it!");
+            }
+            return;
+
         }
     }
 }
